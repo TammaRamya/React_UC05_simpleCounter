eccc732833861c7900d8c25e87ed1889aa044691
TammaRamya <84433894+TammaRamya@users.noreply.github.com>
2021-06-11 14:25:24 +0530

    Update Counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -1,46 +1,42 @@
 import React from 'react';
 export default class Counter extends React.Component {
-    constructor(props)
-    {
+    constructor(props){
         super(props)
         this.state={
             count:0,
             colour:'black'
-        }
-    }
-    increment(){
-        this.setState(
+        } }
+    onClickButton=(event)=>{
+        if(event.target.value==='Increase') {
+            this.setState(
+                {
+               count:this.state.count+1,
+                colour:this.state.count>=0?'green':'red'
+                } )  }
+        else if(event.target.value==='Decrease'){
+            this.setState(
+             {
+                count:this.state.count-1,
+                colour:this.state.count<=0?'red':'green'
+            })}
+        else if(event.target.value==='Reset'){
+            this.setState(
             {
-            count:this.state.count+1,
-            colour:this.state.count>=0?this.state.colour='green':this.state.colour='red'
-            }
-            )
-    }
-    decrement(){
-        this.setState(
-            {
-            count:this.state.count-1,
-            colour:this.state.count<=0?this.state.colour='red':this.state.colour='green'
-            }
-        )
-    }
-    reset(){
-        this.setState(
-            {
-            count:0,
-            colour:this.state.colour='black'
-            }
-        )
+                count:0,
+                colour:'black'
+                })}
     }
     render(){
+        const {count,colour}=this.state;
         return(
+           
             <div>
                 <div style={{fontSize:'30px'}}>Counter</div>
-                <div style={{fontSize:'70px',color:this.state.colour}}>{this.state.count}</div>
+                <div style={{fontSize:'70px',color:colour}}>{count}</div>
                 
-                <button onClick={()=>this.increment()}  >Increment</button>
-                <button onClick={()=>this.decrement()}>Decrement</button>
-                <button onClick={()=>this.reset()}>Reset</button>
+                <button value="Increase" onClick={e=>this.onClickButton(e,'value')} >Increment</button>
+                <button  value="Decrease"onClick={e=>this.onClickButton(e,'value')}>Decrement</button>
+                <button value="Reset" onClick={e=>this.onClickButton(e,'value')}>Reset</button>
             </div>
         )
     }
